77cd305e8a42bcb70f8438700544d72dbf474386
justinbergkamp <justin.bergkamp@gmail.com>
2020-12-19 16:41:48 -0800

    Moved app code to dashboard page

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { APIService } from '../API.service';
+import { Book } from '../../types/book';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +11,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public createForm: FormGroup;
+  mode = '';
 
-  ngOnInit(): void {
+  books: Array<Book>;
+  myFlagForButtonToggle: String = "Single";
+  endpointToggleOptions: Array<String> = ["To-Read", "Read"];
+
+  selectedBook: Book;
+
+
+  constructor(private api: APIService, private fb: FormBuilder) { }
+
+  async ngOnInit() {
+    this.createForm = this.fb.group({
+      'title': ['', Validators.required],
+      'description': ['', Validators.required],
+      'author': ['', Validators.required],
+      'status': ['', Validators.required]
+
+    });
+
+    this.api.ListBooks().then(event => {
+      this.books = event.items;
+      this.books.sort((a, b) => a.queue_pos < b.queue_pos ? -1 : a.queue_pos > b.queue_pos ? 1 : 0)
+
+    });
+
+
+    /* subscribe to new restaurants being created */
+    this.api.OnCreateBookListener.subscribe( (event: any) => {
+      const newBook = event.value.data.onCreateBook;
+      this.books = [newBook, ...this.books];
+      this.books.sort((a, b) => a.queue_pos < b.queue_pos ? -1 : a.queue_pos > b.queue_pos ? 1 : 0)
+
+    });
+
+  }
+  addBook(){
+    this.mode = 'add';
+  }
+
+  updateOrder(): void {
+    let  ind = 1;
+    for (var book of this.books){
+      let updatedBook = {"id":book.id, "title":book.title, "description":book.description, "author":book.author, "status":book.status, "queue_pos":ind};
+      book.queue_pos = ind;
+      this.api.UpdateBook(updatedBook).then(event => {
+        console.log('Order Updated');
+      })
+      .catch(e => {
+        console.log('error updating order', e);
+      });
+      ind++;
+    }
+  }
+
+
+  onSelect(book: Book): void {
+    this.mode = 'details';
+    this.selectedBook = book;
+  }
+
+  public onFilterChange(val){
+    let filteredBooks = this.books.filter(book => book.status == val)
+    this.books = filteredBooks;
+    console.log(val)
+  }
+
+  public onCreate(book: Book) {
+    console.log(book)
+    book.queue_pos = this.books.length+1;
+    this.api.CreateBook(book).then(event => {
+      console.log('item created!');
+      this.createForm.reset();
+    })
+    .catch(e => {
+      console.log('error creating restaurant...', e);
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    console.log(this.books)
+    moveItemInArray(this.books, event.previousIndex, event.currentIndex);
+    console.log(this.books)
+    this.updateOrder();
   }
 
 }
