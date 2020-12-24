53990c71cc3be7a1c8197a0fe24bd796b44daa6d
justinbergkamp <justin.bergkamp@gmail.com>
2020-12-23 22:54:14 -0800

    adding books to library screen

--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -9,11 +9,17 @@ import { Book } from '../../types/book';
   styleUrls: ['./library.component.css']
 })
 export class LibraryComponent implements OnInit {
+  mode = '';
 
   books: Array<Book>;
   toReadBooks: Array<Book>;
   queuedBooks: Array<Book>;
   completedBooks: Array<Book>;
+
+  myFlagForButtonToggle: Array<String> = [];
+  endpointToggleOptions: Array<String> = ["To-Read", "Read"];
+
+  selectedBook: Book;
 
   constructor(private api: APIService) { }
 
@@ -28,6 +34,28 @@ export class LibraryComponent implements OnInit {
       this.books.sort((a, b) => a.queue_pos < b.queue_pos ? -1 : a.queue_pos > b.queue_pos ? 1 : 0)
 
     });
+  }
+
+  onSelect(book: Book): void {
+    this.mode = 'details';
+    this.selectedBook = book;
+  }
+
+  public onFilterChange(val){
+
+    let selectedBooks = [];
+    if (val !=''){
+      for(let filter of val){
+
+        let filteredBooks = this.books.filter(book => book.status == filter);
+
+        selectedBooks = selectedBooks.concat(filteredBooks);
+
+      }
+    }else{
+      selectedBooks = this.books;
+    }
+    this.books = selectedBooks;
   }
 
 }
