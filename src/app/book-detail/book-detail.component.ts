e94cab4a518b6e6c5f43f14a2a0e24d81df1e0e7
justinbergkamp <justin.bergkamp@gmail.com>
2021-01-02 15:46:10 -0800

    Converting details back to form

--- src/app/book-detail/book-detail.component.ts
+++ src/app/book-detail/book-detail.component.ts
@@ -3,6 +3,11 @@ import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { APIService } from '../API.service';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {MatAutocompleteSelectedEvent, MatAutocomplete} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-book-detail',
@@ -10,28 +15,65 @@ import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms'
   styleUrls: ['./book-detail.component.css']
 })
 
-
-
-
 export class BookDetailComponent implements OnInit {
 
 
   @Input() book: Book;
-
-
   statusOptions: Array<String> = ["Backlog", "Groomed", "Queued",  "Current", "Done"];
+  public updateForm: FormGroup;
 
 
-  constructor(private api: APIService) {
+ // information needed for the tags
+  visible = true;
+  selectable = true;
+  removable = true;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  filteredTags: Observable<string[]>;
+  currentTags: string[] = [];
+  allTags: string[] = ['Science', 'Fantasy', 'History', 'Philosophy', 'Self-Improvement'];
+
+  @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+
+
+  constructor(private api: APIService, private fb: FormBuilder) {
+    //
+    // this.updateForm = this.fb.group({
+    //   'title': ['', Validators.required],
+    //   'description': ['', Validators.required],
+    //   'author': ['', Validators.required],
+    //   'tags': [''],
+    //   'status': ['', Validators.required]
+    //
+    // });
+
+    this.updateForm = this.fb.group({
+      'title': ['', Validators.required],
+      'author': ['', Validators.required],
+      'description': [''],
+      'pageNumber': [''],
+      'tags': [''],
+      'status': ['', Validators.required]
+
+    });
+
+    this.filteredTags = this.updateForm.controls.tags.valueChanges.pipe(
+            startWith(null),
+            map((tag: string | null) => tag ? this._filter(tag) : this.allTags.slice()));
 
   }
 
   ngOnInit(): void {
 
+    this.updateForm.patchValue({title: this.book.title, author: this.book.author});
+
   }
 
+
   updateBook(book: Book){
+    console.log("JUSTIN")
     console.log(this.book);
+    console.log(book);
     let updatedBook = {"id":this.book.id, "title":this.book.title, "description":this.book.description, "author":this.book.author, "status":this.book.status, "queue_pos":this.book.queue_pos, "tags":[]};
 
     this.api.UpdateBook(updatedBook).then(event => {
@@ -46,8 +88,46 @@ export class BookDetailComponent implements OnInit {
   public onStatusUpdate(val){
 
     this.book.status = this.statusOptions.indexOf(val);
-;
     console.log(this.book.status );
 
+  }
+
+  //tag methods
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.currentTags.push(value.trim());
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+
+    this.updateForm.controls.tags.setValue(null);
+  }
+
+  remove(fruit: string): void {
+    const index = this.currentTags.indexOf(fruit);
+
+    if (index >= 0) {
+      this.currentTags.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.currentTags.push(event.option.viewValue);
+    this.tagInput.nativeElement.value = '';
+    this.updateForm.controls.tags.setValue(null);
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.allTags.filter(tag => tag.toLowerCase().indexOf(filterValue) === 0);
   }
 }
